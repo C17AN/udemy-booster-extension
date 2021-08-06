0a80daa3b12d77dc674fb3bc1b48cae58699047e
c17an <kimchanmin1@gmail.com>
2021-08-06 11:22:10 +0900

    style: skyblue 계열 색상 변수 톤 조정

--- src/components/Scheduler.vue
+++ src/components/Scheduler.vue
@@ -1,5 +1,8 @@
 <template>
   <Calendar is-expanded />
+  <div class="schedule-btn-container">
+    <button class="set-goal-btn">수강 목표 설정하기</button>
+  </div>
 </template>
 
 <script>
@@ -11,4 +14,24 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped lang="scss">
+.schedule-btn-container {
+  display: flex;
+  justify-content: flex-end;
+  margin-top: 14px;
+  .set-goal-btn {
+    padding: 8px 12px;
+    outline: none;
+    background-color: var(--skyblue-300);
+    color: white;
+    border: 1px solid #cdcdcd;
+    border-radius: 8px;
+    cursor: pointer;
+    transition: all 0.2s ease;
+    &:hover {
+      transition: all 0.2s ease;
+      background-color: var(--skyblue-500);
+    }
+  }
+}
+</style>
